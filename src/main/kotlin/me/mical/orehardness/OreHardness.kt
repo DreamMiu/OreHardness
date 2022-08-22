14a34e004572e641c72a6953248029ff91bc8103
Micalhl <micalhl@sina.cn>
2022-08-22 09:08:08 +0800

    update

--- src/main/kotlin/me/mical/orehardness/OreHardness.kt
+++ src/main/kotlin/me/mical/orehardness/OreHardness.kt
@@ -5,10 +5,16 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 import taboolib.common.platform.Plugin
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.command
 import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common5.Mirror
+import taboolib.common5.mirrorNow
+import taboolib.module.chat.colored
 import taboolib.module.configuration.Config
 import taboolib.module.configuration.Configuration
 import taboolib.module.nms.getI18nName
+import taboolib.platform.util.modifyMeta
 import taboolib.platform.util.sendActionBar
 
 object OreHardness : Plugin() {
@@ -24,30 +30,40 @@ object OreHardness : Plugin() {
                 oreMap.putIfAbsent(Material.getMaterial(it)!!, conf.getInt(it, 1))
             }
         }
+
+        command("orehardness") {
+            literal("report") {
+                execute<ProxyCommandSender> { user, _, _ ->
+                    Mirror.report(user)
+                }
+            }
+        }
     }
 
     @SubscribeEvent
     fun e(e: BlockBreakEvent) {
-        if (oreMap.containsKey(e.block.type)) {
-            val durability = oreMap[e.block.type] ?: return
-            val item = e.player.itemInUse ?: return
-            val meta = item.itemMeta!!
-            val name = ItemStack(e.block.type).getI18nName()
-            if (meta is Damageable) {
-                if (item.type.maxDurability.toInt() - meta.damage < durability) {
-                    meta.damage = item.type.maxDurability.toInt()
-                    item.itemMeta = meta
-                    e.player.sendActionBar("&e你尝试开采 &b$name, 但你的工具报废了也没能挖掉该方块...")
-                    e.isCancelled = true
-                } else {
-                    meta.damage = meta.damage + durability
-                    item.itemMeta = meta
-                    if (durability in (1..3)) {
-                        e.player.sendActionBar("&e你开采的 &b$name &e矿物硬度适中, 消耗了你${durability}格耐久.")
-                    } else if (durability in 4..7) {
-                        e.player.sendActionBar("&e你开采的 &b$name &e矿物稍微有点硬, 消耗了你${durability}格耐久.")
-                    } else {
-                        e.player.sendActionBar("&e你开采的 &b$name &e矿物硬度过大! 消耗了你${durability}格耐久.")
+        mirrorNow("OreHardness:Handler:BlockBreak") {
+            if (oreMap.containsKey(e.block.type)) {
+                val durability = oreMap[e.block.type] ?: return@mirrorNow
+                val item = e.player.equipment?.itemInMainHand ?: return@mirrorNow
+                val name = ItemStack(e.block.type).getI18nName()
+                if (item.type.name.contains("PICKAXE")) {
+                    item.modifyMeta<Damageable> {
+                        if (item.type.maxDurability.toInt() - damage < durability) {
+                            // FIXME: 这样有些简单粗暴, 我更希望的是有原版工具坏掉的动画.
+                            item.amount = 0
+                            e.player.sendActionBar("&e你尝试开采 &b$name, 但你的工具报废了也没能挖掉该方块...".colored())
+                            e.isCancelled = true
+                        } else {
+                            damage += durability
+                            if (durability in (1..3)) {
+                                e.player.sendActionBar("&e你开采的 &b$name &e矿物硬度适中, 消耗了你${durability}格耐久.".colored())
+                            } else if (durability in 4..7) {
+                                e.player.sendActionBar("&e你开采的 &b$name &e矿物稍微有点硬, 消耗了你${durability}格耐久.".colored())
+                            } else {
+                                e.player.sendActionBar("&e你开采的 &b$name &e矿物硬度过大! 消耗了你${durability}格耐久.".colored())
+                            }
+                        }
                     }
                 }
             }
